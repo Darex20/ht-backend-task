2adab67f278e72a368a6bc53d7866e5812557f54
Darex20 <pava.dario@gmail.com>
2023-06-11 16:32:33 +0200

    Add tests for Controller and Service

--- src/test/java/ht/backend/controller/ShipmentTrackingControllerTest.java
+++ src/test/java/ht/backend/controller/ShipmentTrackingControllerTest.java
@@ -1,32 +1,169 @@
 package ht.backend.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.fge.jsonpatch.JsonPatchException;
+import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
+import ht.backend.enums.ErrorSeverity;
+import ht.backend.model.CustomError;
+import ht.backend.model.ShipmentTracking;
+import ht.backend.service.ShipmentTrackingService;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 class ShipmentTrackingControllerTest {
 
-    @Test
-    void getShipmentTracking() {
+    private ShipmentTrackingController shipmentTrackingController;
+
+    @Mock
+    private ShipmentTrackingService shipmentTrackingService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        shipmentTrackingController = new ShipmentTrackingController(shipmentTrackingService);
     }
 
     @Test
-    void getShipmentTrackingById() {
+    void testGetShipmentTracking() {
+        // Arrange
+        List<ShipmentTracking> expectedShipmentTrackingList = new ArrayList<>();
+        when(shipmentTrackingService.getAllShipmentTracking()).thenReturn(expectedShipmentTrackingList);
+
+        // Act
+        List<ShipmentTracking> result = shipmentTrackingController.getShipmentTracking();
+
+        // Assert
+        assertEquals(expectedShipmentTrackingList, result);
+        verify(shipmentTrackingService, times(1)).getAllShipmentTracking();
     }
 
     @Test
-    void getShipmentTrackingByStatus() {
+    void testGetShipmentTrackingById_WhenIdExists() {
+        // Arrange
+        Long id = 1L;
+        ShipmentTracking expectedShipmentTracking = new ShipmentTracking();
+        when(shipmentTrackingService.getShipmentTrackingById(id)).thenReturn(expectedShipmentTracking);
+
+        // Act
+        ResponseEntity<?> responseEntity = shipmentTrackingController.getShipmentTrackingById(id);
+
+        // Assert
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(expectedShipmentTracking, responseEntity.getBody());
+        verify(shipmentTrackingService, times(1)).getShipmentTrackingById(id);
     }
 
     @Test
-    void getShipmentTrackingDate() {
+    void testGetShipmentTrackingById_WhenIdDoesNotExist() {
+        // Arrange
+        Long id = 1L;
+        EntityNotFoundException exception = new EntityNotFoundException("ShipmentTracking not found");
+        when(shipmentTrackingService.getShipmentTrackingById(id)).thenThrow(exception);
+
+        // Act
+        ResponseEntity<?> responseEntity = shipmentTrackingController.getShipmentTrackingById(id);
+
+        // Assert
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+        assertTrue(responseEntity.getBody() instanceof CustomError);
+        assertEquals(ErrorSeverity.FATAL, ((CustomError) responseEntity.getBody()).getSeverity());
+        verify(shipmentTrackingService, times(1)).getShipmentTrackingById(id);
+    }
+
+    // Additional unit tests for the other methods in the controller...
+
+    @Test
+    void testAddShipmentTracking() {
+        // Arrange
+        ShipmentTracking shipmentTracking = new ShipmentTracking();
+        ShipmentTracking savedShipmentTracking = new ShipmentTracking();
+        when(shipmentTrackingService.addNewShipmentTracking(shipmentTracking)).thenReturn(savedShipmentTracking);
+
+        // Act
+        ResponseEntity<ShipmentTracking> responseEntity = shipmentTrackingController.addShipmentTracking(shipmentTracking);
+
+        // Assert
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertEquals(savedShipmentTracking, responseEntity.getBody());
+        assertEquals(URI.create("/" + savedShipmentTracking.getId()), responseEntity.getHeaders().getLocation());
+        verify(shipmentTrackingService, times(1)).addNewShipmentTracking(shipmentTracking);
     }
 
     @Test
-    void addShipmentTracking() {
+    void testUpdateShipment_WhenSuccessful() throws JsonPatchException, JsonProcessingException {
+        // Arrange
+        Long id = 1L;
+        ShipmentTracking shipmentTracking = new ShipmentTracking();
+        JsonMergePatch patch = mock(JsonMergePatch.class); // Mocking the JsonMergePatch object
+        ShipmentTracking patchedShipmentTracking = new ShipmentTracking();
+        when(shipmentTrackingService.getShipmentTrackingById(id)).thenReturn(shipmentTracking);
+        when(shipmentTrackingService.applyPatchToShipmentTracking(any(), eq(shipmentTracking))).thenReturn(patchedShipmentTracking);
+
+        // Act
+        ResponseEntity<?> responseEntity = shipmentTrackingController.updateShipment(id, patch);
+
+        // Assert
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(patchedShipmentTracking, responseEntity.getBody());
+        verify(shipmentTrackingService, times(1)).getShipmentTrackingById(id);
+        verify(shipmentTrackingService, times(1)).applyPatchToShipmentTracking(any(), eq(shipmentTracking));
+        verify(shipmentTrackingService, times(1)).updateShipmentTracking(patchedShipmentTracking);
     }
 
     @Test
-    void updateShipment() {
+    void testUpdateShipment_WhenJsonPatchExceptionOccurs() throws JsonPatchException, JsonProcessingException {
+        // Arrange
+        Long id = 1L;
+        ShipmentTracking shipmentTracking = new ShipmentTracking();
+        JsonMergePatch patch = mock(JsonMergePatch.class); // Mocking the JsonMergePatch object
+        JsonPatchException exception = new JsonPatchException("Error applying patch");
+        when(shipmentTrackingService.getShipmentTrackingById(id)).thenReturn(shipmentTracking);
+        doThrow(exception).when(shipmentTrackingService).applyPatchToShipmentTracking(any(), eq(shipmentTracking));
+
+        // Act
+        ResponseEntity<?> responseEntity = shipmentTrackingController.updateShipment(id, patch);
+
+        // Assert
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
+        assertTrue(responseEntity.getBody() instanceof CustomError);
+        assertEquals(ErrorSeverity.FATAL, ((CustomError) responseEntity.getBody()).getSeverity());
+        verify(shipmentTrackingService, times(1)).getShipmentTrackingById(id);
+        verify(shipmentTrackingService, times(1)).applyPatchToShipmentTracking(any(), eq(shipmentTracking));
+    }
+
+    @Test
+    void testUpdateShipment_WhenEntityNotFoundExceptionOccurs() throws JsonPatchException, JsonProcessingException {
+        // Arrange
+        Long id = 1L;
+        ShipmentTracking shipmentTracking = new ShipmentTracking();
+        JsonMergePatch patch = mock(JsonMergePatch.class); // Mocking the JsonMergePatch object
+        EntityNotFoundException exception = new EntityNotFoundException("ShipmentTracking not found");
+        when(shipmentTrackingService.getShipmentTrackingById(id)).thenThrow(exception);
+
+        // Act
+        ResponseEntity<?> responseEntity = shipmentTrackingController.updateShipment(id, patch);
+
+        // Assert
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+        assertTrue(responseEntity.getBody() instanceof CustomError);
+        assertEquals(ErrorSeverity.FATAL, ((CustomError) responseEntity.getBody()).getSeverity());
+        verify(shipmentTrackingService, times(1)).getShipmentTrackingById(id);
+        // Verify that the applyPatchToShipmentTracking method is not invoked
+        verify(shipmentTrackingService, never()).applyPatchToShipmentTracking(any(), any());
     }
 }
--- src/test/java/ht/backend/service/ShipmentTrackingServiceTest.java
+++ src/test/java/ht/backend/service/ShipmentTrackingServiceTest.java
@@ -1,4 +1,123 @@
 package ht.backend.service;
 
-public class ShipmentTrackingServiceTest {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ht.backend.enums.ShipmentStatus;
+import ht.backend.model.ShipmentTracking;
+import ht.backend.repository.ShipmentTrackingRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+class ShipmentTrackingServiceTest {
+
+    private ShipmentTrackingService shipmentTrackingService;
+
+    @Mock
+    private ShipmentTrackingRepository shipmentTrackingRepository;
+
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        shipmentTrackingService = new ShipmentTrackingService(shipmentTrackingRepository, objectMapper);
+    }
+
+    @Test
+    void testGetAllShipmentTracking() {
+        // Arrange
+        List<ShipmentTracking> expectedShipmentTrackingList = new ArrayList<>();
+        when(shipmentTrackingRepository.findAll()).thenReturn(expectedShipmentTrackingList);
+
+        // Act
+        List<ShipmentTracking> result = shipmentTrackingService.getAllShipmentTracking();
+
+        // Assert
+        assertEquals(expectedShipmentTrackingList, result);
+        verify(shipmentTrackingRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testAddNewShipmentTracking() {
+        // Arrange
+        ShipmentTracking shipmentTracking = new ShipmentTracking();
+        when(shipmentTrackingRepository.save(shipmentTracking)).thenReturn(shipmentTracking);
+
+        // Act
+        ShipmentTracking result = shipmentTrackingService.addNewShipmentTracking(shipmentTracking);
+
+        // Assert
+        assertEquals(shipmentTracking, result);
+        verify(shipmentTrackingRepository, times(1)).save(shipmentTracking);
+    }
+
+    @Test
+    void testGetShipmentTrackingById_WhenIdExists() {
+        // Arrange
+        Long id = 1L;
+        ShipmentTracking expectedShipmentTracking = new ShipmentTracking();
+        Optional<ShipmentTracking> optionalShipmentTracking = Optional.of(expectedShipmentTracking);
+        when(shipmentTrackingRepository.findById(id)).thenReturn(optionalShipmentTracking);
+
+        // Act
+        ShipmentTracking result = shipmentTrackingService.getShipmentTrackingById(id);
+
+        // Assert
+        assertEquals(expectedShipmentTracking, result);
+        verify(shipmentTrackingRepository, times(1)).findById(id);
+    }
+
+    @Test
+    void testGetShipmentTrackingById_WhenIdDoesNotExist() {
+        // Arrange
+        Long id = 1L;
+        Optional<ShipmentTracking> optionalShipmentTracking = Optional.empty();
+        when(shipmentTrackingRepository.findById(id)).thenReturn(optionalShipmentTracking);
+
+        // Act & Assert
+        assertThrows(EntityNotFoundException.class, () -> shipmentTrackingService.getShipmentTrackingById(id));
+        verify(shipmentTrackingRepository, times(1)).findById(id);
+    }
+
+    // Additional unit tests for the other methods in the service...
+
+    @Test
+    void testGetAllShipmentTrackingByStatus_WhenStatusExists() {
+        // Arrange
+        ShipmentStatus shipmentStatus = ShipmentStatus.SHIPPED;
+        List<ShipmentTracking> expectedList = new ArrayList<>();
+        expectedList.add(new ShipmentTracking());
+        when(shipmentTrackingRepository.findAllByStatus(shipmentStatus)).thenReturn(Optional.of(expectedList));
+
+        // Act
+        List<ShipmentTracking> result = shipmentTrackingService.getAllShipmentTrackingByStatus(shipmentStatus);
+
+        // Assert
+        assertEquals(expectedList, result);
+        verify(shipmentTrackingRepository, times(1)).findAllByStatus(shipmentStatus);
+    }
+
+    @Test
+    void testGetAllShipmentTrackingByStatus_WhenStatusDoesNotExist() {
+        // Arrange
+        ShipmentStatus shipmentStatus = ShipmentStatus.SHIPPED;
+        when(shipmentTrackingRepository.findAllByStatus(shipmentStatus)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(EntityNotFoundException.class, () -> shipmentTrackingService.getAllShipmentTrackingByStatus(shipmentStatus));
+        verify(shipmentTrackingRepository, times(1)).findAllByStatus(shipmentStatus);
+    }
+
 }
+
